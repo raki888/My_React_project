2d972be4d828b73344f255efe699f3b69d5eea3d
raki <rakeshpatel.esampelly503@gmail.com>
2025-04-29 18:09:08 +0530

    Initial commit with .gitignore in place

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🚀 Hello from My React App!</h1>
+      <p>This is a basic React app deployed to GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
